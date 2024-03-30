e7e6d5734b3700be864bd90a3805b0777c8bedd5
wrbrant <wrbrant23@gmail.com>
2024-03-30 14:02:47 -0600

    roughing in better command help

---
+++ scripts/custom-command.go
@@ -0,0 +1,98 @@
+package main
+
+import (
+	"fmt"
+	"io/ioutil"
+	"os"
+	"os/user"
+	"path/filepath"
+	"regexp"
+	"strings"
+)
+
+var helpString = `
+=====custom-command help=====
+Helps organize custom commands -- all user made aliases and scripts on the system
+
+Flags:
+-A, --all		prints all aliases and scripts
+-s, --scripts	prints all scripts
+-a, --aliases	prints all aliases
+-u, --unused	prints all aliases and scripts that haven't been used in awhile
+-h, --help		help for custom-command
+`
+
+var cmds []byte
+var fpath_bash_history string
+var dpath_bin string
+var commands []string
+var all bool
+var scripts bool
+var aliases bool
+
+func main() {
+	currUser, err := user.Current()
+	chk(err)
+	fpath_bash_history = filepath.Join(currUser.HomeDir, ".bash_history")
+	dpath_bin = filepath.Join(currUser.HomeDir, "bin")
+
+	args := os.Args[1:]
+	if len(args) == 0 {
+		fmt.Printf(helpString)
+	}
+	for _, arg := range args {
+		switch arg {
+		case "-A", "--all":
+		case "-s", "--scripts":
+			getScripts()
+		case "-a", "--aliases":
+		case "-u", "--unused":
+			getBashHistory()
+			printUnused()
+		case "-h", "--help":
+		}
+	}
+
+}
+
+func getBashHistory() {
+	if _, err := os.Stat(fpath_bash_history); err != nil {
+		fmt.Println(fpath_bash_history + " did not exist")
+		os.Exit(0)
+	}
+	var err error
+	cmds, _ = os.ReadFile(fpath_bash_history)
+	chk(err)
+}
+
+func getScripts() {
+	items, err := ioutil.ReadDir(dpath_bin)
+	chk(err)
+	for _, item := range items {
+		if !strings.Contains(item.Name(), ".") && !(item.Name() == "bash_aliases") && !item.IsDir() {
+			commands = append(commands, item.Name())
+		}
+	}
+}
+
+func getAliases() {
+
+}
+
+func printUnused() {
+	var unfound []string
+	for _, cmd := range commands {
+		if found := regexp.MustCompile(fmt.Sprintf(`\n\s*%s\s*\n`, cmd)).Find(cmds); found == nil {
+			unfound = append(unfound, cmd)
+		}
+	}
+	if len(unfound) > 0 {
+		fmt.Printf("The following scripts haven't been used in awhile:\n  * %s\n", strings.Join(unfound, "\n  * "))
+	}
+}
+
+func chk(err error) {
+	if err != nil {
+		panic(err)
+	}
+}
